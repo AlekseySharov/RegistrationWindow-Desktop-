4413e54010ad51ac75131cf37508ce02f8cf0c48
AlekseySharov <122973778+AlekseySharov@users.noreply.github.com>
2023-11-12 17:08:08 +0300

    Update Configs.java

--- DesktopApplication/src/main/java/example/desktopapplication/Configs.java
+++ DesktopApplication/src/main/java/example/desktopapplication/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected  String dbPass = "kbvjy123";
+    protected  String dbPass = "washparol";
     protected String dbName = "desktop";
 }
